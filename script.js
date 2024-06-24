ba3f86fd5c2739781bf76438e1d06eb0510048bf
abi febrian tw <abifebriantw@gmail.com>
2024-06-24 19:03:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const todoInput = document.querySelector(".todo-input");
-const todoLevel = document.querySelector(".todo-level")
 const todoDate = document.querySelector(".todo-date");
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
@@ -10,6 +11,79 @@ todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("change", filterTodo);
 
+/**
+ * @param {Date} date
+ */
+function formatTime(date) {
+    const hours12 = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes();
+    const isAm = date.getHours() < 12;
+  
+    return `${hours12.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+  }
+  
+  /**
+   * @param {Date} date
+   */
+  function formatDate(date) {
+    const DAYS = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    const MONTHS = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+  
+    return `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} 
+    ${date.getDate()} ${date.getFullYear()}`;
+  }
+  
+  setInterval(() => {
+    const now = new Date();
+  
+    timeElement.textContent = formatTime(now);
+    dateElement.textContent = formatDate(now);
+  }, 200);
+
+        var d = new Date()
+        var yr = d.getFullYear();
+        var month = d.getMonth()+1;
+
+        if(month<10){
+            month = '0'+month
+        }
+
+        var date=d.getDate();
+        if(date<10){
+            date='0'+date
+        }
+
+        var c_date = yr+"-"+month+"-"+date;
+        document.getElementById('d1').value=c_date;
+
+level.addEventListener('change', showCapital);
+function showCapital(){
+    capital.value = level.value;
+}
+
 function addTodo(event) {
     event.preventDefault();
     const todoDiv = document.createElement("div");
@@ -17,16 +91,15 @@ function addTodo(event) {
     const newTodo = document.createElement("li");
     newTodo.innerHTML = 
         newTodo.innerHTML + 
-            `<div class="task">
-            <h3>${todoInput.value}</h3>
-            <p class="seven">${todoLevel.value}</p>
-            <p class="seven">${todoDate.value}</p>
+            `<div class="oye">
+            <h6>Ditulis      : ${d1.value},Prioritas    :  ${capital.value}, Dikerjakan  :${todoDate.value}</h6>
+            <br>
+            <h3>Tugas       : ${todoInput.value}</h3>
             </div>`
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
     //ADDING TO LOCAL STORAGE 
     saveLocalTodos(todoInput.value);
-    saveLocalTodos(todoLevel.value);
     saveLocalTodos(todoDate.value);
     
     const completedButton = document.createElement("button");
@@ -41,8 +114,8 @@ function addTodo(event) {
     
     todoList.appendChild(todoDiv);
     todoInput.value = "";
-    todoLevel.value = "";
     todoDate.value = "";
+    
 }
 
 function deleteCheck(e) {
